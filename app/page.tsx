2c8e006807f7b8e2048d4be95be0892a654d3f10
shamil-ch <shamilkolekkara@gmail.com>
2023-12-29 16:29:53 +0530

    corrected typos

--- app/page.tsx
+++ app/page.tsx
@@ -44,6 +44,7 @@ export default function Page() {
             width={560}
             height={620}
             className = "block md:hidden"
+            alt="mobile Hero image"
           />
         </div>
       </div>
